be68e76645ef4ac38e5ad3a94d49d8f52c7e6617
42222 <50253762+42222@users.noreply.github.com>
2025-11-17 12:58:06 -0800

    Create sample_data.sql

---
+++ sql/sample_data.sql
@@ -0,0 +1,13 @@
+-- Sample data for CS4222 University Database Project
+-- TODO: Add INSERT statements for testing.
+
+-- Example:
+-- INSERT INTO Department(dept_id, dept_name, main_office) VALUES
+-- (1, 'Computer Science', 'E&T 200'),
+-- (2, 'Mathematics', 'KH 300');
+
+-- INSERT INTO Professor(...) VALUES (...);
+-- INSERT INTO GradStudent(...) VALUES (...);
+-- INSERT INTO Project(...) VALUES (...);
+-- INSERT INTO ProjectFaculty(...) VALUES (...);
+-- INSERT INTO ProjectStudent(...) VALUES (...);
