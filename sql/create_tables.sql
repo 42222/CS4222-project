15ae956cf5650609079f51be35a97c0e2860d82e
42222 <50253762+42222@users.noreply.github.com>
2025-11-17 12:54:55 -0800

    Create create_tables.sql

---
+++ sql/create_tables.sql
@@ -0,0 +1,20 @@
+-- CREATE TABLE statements for CS4222 Database Project
+-- TODO: Fill in with actual schema later.
+
+-- Departments table
+-- CREATE TABLE Department (...);
+
+-- Professors table
+-- CREATE TABLE Professor (...);
+
+-- Graduate Students table
+-- CREATE TABLE GradStudent (...);
+
+-- Projects table
+-- CREATE TABLE Project (...);
+
+-- ProjectFaculty relationship table (PI / CO-PI)
+-- CREATE TABLE ProjectFaculty (...);
+
+-- ProjectStudent relationship table
+-- CREATE TABLE ProjectStudent (...);
