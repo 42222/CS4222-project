961e8c9f5b3692296f21a326eef9c2f456aac7ef
42222 <50253762+42222@users.noreply.github.com>
2025-11-17 12:57:33 -0800

    Create procedures.sql

---
+++ sql/procedures.sql
@@ -0,0 +1,23 @@
+-- Stored Procedures for CS4222 University Database Project
+-- TODO: Add actual PL/pgSQL definitions for required procedures.
+
+-- female_faculty(): return percentage of female professors
+-- Example:
+-- CREATE OR REPLACE FUNCTION female_faculty()
+-- RETURNS NUMERIC AS $$
+-- BEGIN
+--     -- Implementation goes here
+-- END;
+-- $$ LANGUAGE plpgsql;
+
+---------------------------------------------------------------
+
+-- total_people(pno): return number of PI, CO-PI, and students
+-- working on the project with project number pno
+-- Example:
+-- CREATE OR REPLACE FUNCTION total_people(pno INTEGER)
+-- RETURNS INTEGER AS $$
+-- BEGIN
+--     -- Implementation goes here
+-- END;
+-- $$ LANGUAGE plpgsql;
