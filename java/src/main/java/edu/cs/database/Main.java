295f850cdc56ecee28bcbeeecc8b2c26b0775a49
42222 <50253762+42222@users.noreply.github.com>
2025-11-30 22:57:45 -0800

    Update Main.java

--- java/src/main/java/edu/cs/database/Main.java
+++ java/src/main/java/edu/cs/database/Main.java
@@ -1,7 +1,16 @@
 package edu.cs.database;
 
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("University DB Project - JDBC Client Started");
+
+        try (Connection conn = DBConnection.getConnection()) {
+            System.out.println("Connected to cs1.calstatela.edu successfully!");
+        } catch (Exception e) {
+            System.out.println("Failed to connect to database:");
+            e.printStackTrace();
+        }
     }
 }
